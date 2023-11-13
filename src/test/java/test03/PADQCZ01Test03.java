6d777b835c82ef0ddf787841f9ea9a171ecbc7b2
Lucki Alan Fernandez Salcedo <alan.Fernandez@bbva.com>
2023-11-13 13:05:06 -0500

    test rendimiento

--- src/test/java/test03/PADQCZ01Test03.java
+++ src/test/java/test03/PADQCZ01Test03.java
@@ -15,6 +15,7 @@ public class PADQCZ01Test03 {
         console("");
 
     }
+
     @After
     public void tearDown() {
         console("");
@@ -24,11 +25,39 @@ public class PADQCZ01Test03 {
         console("");
 
     }
+
     @Test
-    public void executeTest01(){
+    public void executeTest01() {
+
+        long start = System.currentTimeMillis();
+        System.out.println( "start = " + start );
+
+        int n = 100;
+
+        for (int i = 0; i < n; i++) {
+            //System.out.println( i );
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    for (int l = 0; l < n; l++) {
+                        for (int m = 0; m < n; m++) {
+                            for (int o = 0; o < n; o++) {
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        long finish = System.currentTimeMillis();
+        System.out.println( "finish = " + finish );
+
+        long total = finish - start;
+        System.out.println( "total = " + total/1000.0 );
 
     }
-    void console(String x){
+
+    void console(String x) {
         System.out.println(x);
     }
 }
